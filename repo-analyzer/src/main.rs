6ebd735ed70783d8681c90f70ed506edeac35048
slidwell44 <slidwell@gmail.com>
2025-10-14 21:56:03 -0400

    3.1 Variables and Mutability

--- repo-analyzer/src/main.rs
+++ repo-analyzer/src/main.rs
@@ -1,34 +1,12 @@
-use rand::Rng;
-use std::cmp::Ordering;
-
 fn main() {
-    println!("Guess the number!");
+    let x = 5;
 
-    let secret_number = rand::thread_rng().gen_range(1..100);
+    let x = x + 1;
 
-    loop {
-        println!("Please input your guess.");
-
-        let mut guess = String::new();
-
-        std::io::stdin()
-            .read_line(&mut guess)
-            .expect("Failed to read line");
-
-        let guess: u32 = match guess.trim().parse() {
-            Ok(num) => num,
-            Err(_) => continue,
-        };
-
-        println!("You guessed: {guess}");
-
-        match guess.cmp(&secret_number) {
-            Ordering::Less => println!("Too small!"),
-            Ordering::Greater => println!("Too big!"),
-            Ordering::Equal => {
-                println!("You win!");
-                break;
-            }
-        }
+    {
+        let x = x * 2;
+        println!("The value of x in the inner scope is: {x}");
     }
+
+    println!("The value of x is: {x}");
 }
